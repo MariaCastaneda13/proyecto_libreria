d8bf334f900224cdeca806421b60b310a550e745
MariaCastaneda13 <mariafcastanedaa@gmail.com>
2024-06-12 20:31:31 -0500

    Se agregan y se modifican los handlebars

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 import express from "express";
 import indexRoutes from "./rutas/Index.js";
+import __dirname from "./dirname.js";
+import handlebars from "express-handlebars";
 
 
 const app = express();
-  
-const PORT = 8080;
 
+const PORT = 8080;
+// Handlebars Configuration
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname: "hbs",
+        defaultLayout: "main",
+    })
+);
+
+app.set("view engine", "hbs");
+app.set("views", `${__dirname}/views`);
+
+
+
+//Endpoints
+app.use("/api", indexRoutes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.send("<h1> Aquí encontrarás una gran variedad de libros de tus géneros favoritos </h1>");
-});
-//Endpoints
-app.use("/api",indexRoutes);
 
-
-app.listen(PORT, () => {
-  console.log(`Leyendo en el puerto http://localhost:${PORT}`);
+//Rutas
+app.get("/", (req, res) => { 
+  const data={ name: "Maria"};
+  res.render("inicio", data);
 });
+app.listen(PORT, ()=>{
+  console.log(`Server running on Port http://localhost:${PORT}`);
+  });
