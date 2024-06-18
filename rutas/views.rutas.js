910ab47a292d068bf65fe5744c3144df40bb8669
MariaCastaneda13 <mariafcastanedaa@gmail.com>
2024-06-18 12:40:36 -0500

    Se ajusta el archivo views.rutas.js

--- rutas/views.rutas.js
+++ rutas/views.rutas.js
@@ -4,11 +4,11 @@ import { productManager } from "../managers/ProductManager.js";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { productManager });
+  res.render("index", {productManager});
 });
 
-router.get("/realtimeproducts", (req, res) => {
-  res.render("/realtimeproducts");
+router.get("/realTimeProducts", (req, res) => {
+  res.render("realTimeProducts");
 });
 
 export default router;
